5f0ca5b5a69b4c12fe47185d0a2aa249a899e914
Xavier Pashley <xavipashley@gmail.com>
2023-11-14 11:40:19 +0000

    Activities.js

---
+++ Activities.js
@@ -0,0 +1,520 @@
+
+Day 1 Activity 1
+
+// let Friend1 = 'Xavi';
+// let Age1 = 21;
+// let Colour1 = 'Purple';
+
+// console.log(`Hi my name is  ${Friend1} . I am  ${Age1}  years old. I like to drink ${Colour1}`)
+
+
+
+
+
+
+Day 1 Activity 2
+
+// let iAte = [
+//     "Xavi - Cheese",
+//     "Nora - salmon bagel",
+//     "Safeer - cheese sandwich",
+// ];
+// console.log(iAte);
+
+
+
+
+
+
+Day 1 Activity 3
+
+// LOG Days to Birthday
+// let dateToday = new Date();
+// let myBirthdate = new Date('2024, 3, 27');
+ 
+// let milliseconds = myBirthdate - dateToday;
+// milliseconds = milliseconds / 1000;
+// milliseconds /= 60;
+// milliseconds /= 60;
+// milliseconds /= 24;
+
+
+
+
+
+
+Day 1 Activity 4
+
+// var array1 = Array.from(new Array(5));
+
+// console.log(array1[0]);
+// console.log(array1[1]);
+// console.log(array1[2]);
+// console.log(array1[3]);
+// console.log(array1[4]);
+
+// console.log(Object.getownpro)
+
+// console.log("   |   |   ");
+// console.log(" X | O |   ");
+// console.log("   |   |   ");
+// console.log("-----------");
+// console.log("   |   |   ");
+// console.log(" X | X |   ");
+// console.log("   |   |   ");
+// console.log("-----------");
+// console.log("   |   |   ");
+// console.log(" O |   |   ");
+// console.log("   |   |   ");
+
+
+
+
+
+// -------------------------------
+
+
+
+
+Day 2 Activity 1
+
+// age 30;
+
+// let country = "UK";
+
+// if(age > 17 && country == "UK")
+// {
+//    console.log("Yes I can server you");
+// }
+// else
+// {
+//     console.log("You are not old enough");
+// }
+
+
+
+
+
+
+
+Day 2 Activity 2
+
+// let topping = "sweet corn";
+// if(topping == "sweet corn")
+// {
+//     console.log(`I want ${topping} for my pizza`)
+// }
+
+// topping = "Pinapple";
+// if(topping == "Pinapple")
+// {
+//     console.log(`I don't mind ${topping} as my topping on pizza.`)
+// }
+// topping = "Chilli";
+// console.log(`I dont like ${topping} topping on my pizza.`
+
+
+
+
+
+
+Day 2 Activity 3
+
+// let password = "101010110101";
+// if(password.length <8)
+// {
+//     console.log("Your passowrd is short.");
+// }
+// else{
+//     console.log(password);
+// }
+
+
+
+
+
+
+Day 2 Activity 4
+
+// let num = 10;
+
+// if( num % 3 == 0)
+// {
+//     console.log("Bobba");
+// }
+// else if(num % 5 == 0)
+// {
+//     console.log("Fett");
+// }
+//     else if(num % 5 == 0 && num % 3 == 0)
+// {
+//     console.log("BobbaFett")
+// }
+
+
+
+
+
+
+Day 2 Activity 5
+
+// let num = "987654321";
+// let statusy = "is";
+
+// for(let i = 0; i < num.length / 2; i++){
+//     if (num[i] != num[num.length - 1 - i]){
+//         statusy = "not";
+//         break;
+//     }
+// }
+
+// if (statusy== "not") {
+//     console.log("NOT A PALINDROME");
+// }    else{
+//         console.log("IS A PALINDROME");
+//     }
+
+
+
+
+
+
+Day 2 Activity 6
+
+// let time = 7;
+// let placeOfWork = "I am work at the moment";
+// let townOfHome = "Honey.. i'm home!";
+
+// if(time == 7)
+// {
+//     console.log(townOfHome);
+// }
+// else if(time == 8)
+// {
+//     console.log("I am commuting.");
+// }
+// else if(time == 9)
+// {
+//     console.log(placeOfWork);
+// }
+
+
+
+
+
+
+Day 2 Activity 7
+
+// let string = "aiweuydflkwsdxcvhgfdjhiopiwquhedsidfpo";
+
+// console.log("The index of the last vowel is " + (string.length-1));
+
+
+
+
+
+
+
+Day 2 Activity 8
+
+// let word = "practical";
+// let check1 = "true";
+// let check2 = "false";
+// if(word[0] == word[word.length-1])
+// {
+//     console.log("Yes the letters match.");
+//     return true;
+// }
+// else
+// {
+//     console.log("No the letters do not match.");
+//     return false;
+// }
+
+
+
+
+
+
+
+Day 2 Activity 9
+
+// let num1 = 5;
+// let num2 = 8;
+
+// let result1 = num1 + num2;
+// let result2 = num1 * num2;
+
+// if(result1 % 2 == 0)
+// {
+//     console.log(result1);
+// }
+// else
+// {
+//     console.log(`${num1} * ${num2} = ` + result2);
+// }
+
+
+
+
+
+
+
+// -------------------------------
+
+
+
+
+Day 3 Activity 1
+
+// let favShows = [
+//     "Top Boy",
+//     "The Soparanos",
+//     "The Walking Dead",
+//     "The Simpsons",
+//     "The Inbetweeners",
+// ];
+// favShows.push("The Gran Tour");
+// favShows.push("Clarkson's Farm");
+
+
+
+// for(let i = 0; i < favShows.length; i++){
+//     console.log(favShows[i]);
+// }
+
+
+
+
+
+
+Day 3 Activity 2
+
+// let randomNum = [];
+// for(let i = 0; i < 6; i++)
+// {
+//     console.log(Math.floor(Math.random(i)*50))
+// }
+
+
+
+
+
+Day 3 Activity 3
+
+// let randomNum = [];
+// for(let i = 9; i => 0; i--)
+// {
+//     console.log(i);
+// }
+
+
+
+Day 3 Activity 4
+
+// let movies = ["The Shining", "Ip Man", "Ghostbusters", "Pursuit of Happiness"];
+// let currentMovie = "The Shining";
+
+// while (currentMovie !="Ghostbusters"){
+//     console.log(currentMovie);
+//     currentMovie = movies[ Math.floor(Math.random(3)*3)];
+// }
+// if(currentMovie = "Ghostbusters"){
+// console.log("Yay it's Ghostbusters");
+// }
+// else(console.log("Boo! We want Ghostbusters"))
+
+
+
+
+
+Day 3 Activity 5
+
+// let randomNum = [];
+// for(let i = 0; i < 6; i++)
+// {
+//     randomNum[i] = Math.floor(Math.random(i)*30);
+// }
+// for(let i = 0; i < randomNum.length; i++)
+// {
+//    if(randomNum[i] % 7 == 0)
+//    {
+//     console.log(`${randomNum[i]} is divisble by 7`);
+//    }
+//    else{
+//     console.log(`${randomNum[i]} is Not divisble by 7`);
+//    }
+// }
+
+
+
+
+
+Day 3 Activity 6
+
+// let bobsFollowers = [
+//     "Xavier",
+//     "Nora",
+//     "Safeer",
+//     "Brian"
+// ];
+
+// let hannahsFollowers = [
+//     "Makarov",
+//     "Nora",
+//     "Xavier",
+//     "Soap"
+// ];
+
+// for(let i = 0; i < bobsFollowers.length; i++)
+// {
+//     for(let j = 0; j < hannahsFollowers.length; j++)
+//     {
+//         if(hannahsFollowers[j] == bobsFollowers[i])
+//         {
+//             console.log(hannahsFollowers[j]);
+//         }
+//     }
+// }
+
+
+
+Day 3 Activity 7
+
+// "For loops are used whe we already know the number of looping steps and you want to execute the loop at a certain Stage. However great and effective the procces of elemintation can be it can still cause bugs if not imported correctly."
+// "While loops are used when you don't know the number of itterations or changes down the line. These loops only run if the expression evaluates to true, this makes while loops dependent on the expression."
+// "Do loops are used to frequent instructions as long as need to be before the loop is satisfied with the end result. The loop does most of the work which can be bad if you want a specific result."
+// "Do-While loops are used for checking the condition of the code once its been executed. This is helpful as its a simple end result. You only use these loops when you are'nt sure how many times you will execute the code, you will however have to execute it atleast once."
+
+
+
+// -------------------------------
+
+
+Day 4 Activity 1
+
+// const factorial = (n) => {
+//     if ((n === 0) || (n === 1)) {
+//         return 1;
+//     } else {
+//         return (n * factorial (n-1));
+//     }
+// }
+// console.log(Maths.floor(factorial(33)));
+
+
+
+
+
+
+Day 4 Activity 2
+
+// let orderCount = 0;
+// const takeOrder = (topping1, topping2) => {
+//     console.log(`Pizza with ${topping1} and ${topping2}`);
+// orderCount++;
+// }
+
+// takeOrder("Cheese", "Chorizo");
+
+
+
+Day 4 Activity 3
+
+// let XaviersAccount = [55455412, 2942, 1000];
+ 
+// const cashWithdrawal = (accHolder, amount) => {
+//     if (accHolder == XaviersAccount){
+//         if (XaviersAccount[1] == "2942"){
+//             if (XaviersAccount[2] > amount){
+//                 console.log(`Amount has been withdrawn from your account ${XaviersAccount[0]}!`);
+//             }
+//             else {
+//                 console.log("Insufficient funds!");
+//             }
+//         }
+//         else{
+//             console.log("Incorrect PIN!");
+//         }
+//     }
+// }
+ 
+// cashWithdrawal(XaviersAccount, 400);
+
+
+
+Day 5 Activity 1
+
+// const person = {
+//     myName:"Xavier",
+     
+//     sayHi() {
+//         return `This is my name ${this.myName}`;
+//     }  
+// };
+
+// console.log(person.sayHi());
+
+
+
+
+
+Day 5 Activity 2
+
+// const pet = {
+//     petName: "Gizmo",
+//     typeOfPet: "Dog",
+//     Age: "12",
+//     colour: "Black & White",
+    
+//     eat(){
+//         return(`${this.petName} is raveging at this current time.`)
+//     },
+
+//     drink(){
+//         return(`${this.petName} is drinking the blood of the elders at this current time.`)
+//     }
+// };
+// console.log(pet.eat());
+// console.log(pet.drink());
+
+
+
+Day 5 Activity 3
+
+// const cafe = {
+//     name: "XAVIs BROWNIES",
+//     seatingCapacity: 50,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino", 3.00,
+//              "Latte", 2.50,
+//              "Filter coffee", 2.00,
+//              "Tea", 1.50,
+//              "Hot chocolate", 2.00,
+//             ],
+//     meals: [
+//             "Brownie", 2.50,
+//             "Special Soup", 3.00,
+//             "Cupcake", 2.00,
+//             "Sweets", 1.00,
+//             "Chocolate", 1.50,
+//         ],
+//          drinksOrdered(drinks){
+//             for (let i = 0; i < this.drinks.length; i++){
+//                  if (drinks == this.drinks[i]){
+//                 console.log(`The drink ${drinks} costs ${this.drinks[i+1]}.`)
+//             }
+//         }
+//     },
+//     mealsOrdered(meals){
+//         for (let i = 0; i < this.meals.length; i++){
+//             if (meals == this.meals[i]){
+//            console.log(`The meal ${meals} costs ${this.meals[i+1]}.`)
+//         }
+//     }
+// }
+
+// };
+
+// cafe.drinksOrdered("Cappuccino");
+// cafe.mealsOrdered("Brownie");
